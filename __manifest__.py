31df5127ddfd91c3df4881b927ec4d464311da55
psat6 <psat6@penguin>
2021-03-03 10:19:15 +0100

    final

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Cloudalia Educacion - Sales - Filtro comercial",
+
+    'summary': """""",
+
+    'description': """Filtro campo de comercial""",
+
+    'author': "Cloudalia Educacion",
+    'website': "http://www.cloudaliaeducacion.com",
+
+    'category': 'Sales',
+    'version': '11.0.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'hr'],
+    
+    'data': [
+        'views/inherited_form_view.xml',
+    ],
+}
