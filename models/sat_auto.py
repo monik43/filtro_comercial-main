586d124d754dfb997f321817be110a319202fc44
psat6 <psat6@penguin>
2021-03-05 11:31:23 +0100

    prova2env

--- models/sat_auto.py
+++ models/sat_auto.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api
 class repairInherit(models.Model):
     _inherit = 'mrp.repair'
 
-    @api.multi
+    @api.onchange('x_ticket')
     def print_repair_order(self):
         repair = self.env['mrp.repair'].search([])
         print(repair)
