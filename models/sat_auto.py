f7ff7c28fcd6e33648380108cfa9df7f33d6d3d0
psat6 <psat6@penguin>
2021-03-08 11:17:41 +0100

    prova 4 models

--- models/sat_auto.py
+++ models/sat_auto.py
@@ -1,16 +1,2 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
-
-
-class repairInherit(models.Model):
-    _inherit = 'mrp.repair'
-
-    x_ordensat = fields.Many2one(
-    'helpdesk.ticket', string="Orden SAT",
-    default=lambda self: self.env['ir.model.data'].xmlid_to_res_id('helpdesk_ticket.view_record_id'))
-
-class helpdeskInherit(models.Model):
-    _inherit = 'helpdesk.ticket'
-
-    def view_record_id(self):
-        return self.id
