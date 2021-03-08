f23f3b6e210d48291a91948e3c6e35aade414ae4
psat6 <psat6@penguin>
2021-03-08 11:07:36 +0100

    prova 3 models

--- models/sat_auto.py
+++ models/sat_auto.py
@@ -5,18 +5,12 @@ from odoo import models, fields, api
 class repairInherit(models.Model):
     _inherit = 'mrp.repair'
 
-    @api.multi
-    def create_sat_order(self):
-        ticket = helpdeskInherit()._get_actual_record()
-
-        hd = self.env['helpdesk.ticket'].browse(ticket.id)
-        #repair = self.env['helpdesk.ticket'].search(['hd.id', '=', 'name'])
-        self.name = hd.name
+    x_ordensat = fields.Many2one(
+    'helpdesk.ticket', string="Orden SAT",
+    default=lambda self: self.env['ir.model.data'].xmlid_to_res_id('helpdesk_ticket.view_record_id'))
 
 class helpdeskInherit(models.Model):
     _inherit = 'helpdesk.ticket'
 
-    @api.multi
-    def _get_actual_record(self):
-        self.ensure_one()
-        return self
+    def view_record_id(self):
+        return self.id
