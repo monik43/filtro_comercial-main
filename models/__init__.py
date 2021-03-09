e44e27f22ceaaa73e16ac13ba6765c8d593c6374
psat6 <psat6@penguin>
2021-03-09 09:40:29 +0100

    ticked add context + model + fix

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,2 @@
 # -*- coding: utf-8 -*-
-from . import sat_auto
+from . import hd_inherit
--- models/hd_inherit.py
+++ models/hd_inherit.py
@@ -2,8 +2,8 @@
 from odoo import models, fields, api
 
 
-class helpdeskticket(models.Model):
+class ticketInherit(models.Model):
     _inherit: 'helpdesk.ticket'
 
-    orden_sat = fields.Many2one(
+    campo_orden_sat = fields.Many2one(
         'mrp.repair', ondelete='set null', string="Orden SAT")
