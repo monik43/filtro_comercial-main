8d4011e306506d90193aef90d9778cac9c1cc0a7
psat6 <psat6@penguin>
2021-03-09 11:26:52 +0100

    prueba indexado x_sn

--- models/hd_inherit.py
+++ models/hd_inherit.py
@@ -9,5 +9,4 @@ class helpdeskticket(models.Model):
     product_id = fields.Many2one('product.product', ondelete='set null', string="Producto a reparar")
     lot_id = fields.Many2one('stock.production.lot', ondelete='set null', string="Lote/Nº de serie")
     serial_n = fields.Char()
-
-    
+    print(x_sn)
