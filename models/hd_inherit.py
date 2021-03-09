c6dbc6944baa0b0c70c01dbc33cd2e65ec27a29e
psat6 <psat6@penguin>
2021-03-09 12:21:06 +0100

    prueba 2

--- models/hd_inherit.py
+++ models/hd_inherit.py
@@ -9,7 +9,6 @@ class helpdeskticket(models.Model):
     product_id = fields.Many2one('product.product', ondelete='set null', string="Producto a reparar")
     lot_id = fields.Many2one('stock.production.lot', ondelete='set null', string="Lote/Nº de serie")
     
-    for record in self:
-        record['x_sn'] = record.lot_id
+    
 
-    print(lot_id)
+    print(helpdesk.ticket.x_sn)
