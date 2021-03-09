50fe4e5800d75d2d39c70a9ac1fe24fa89007bcb
psat6 <psat6@penguin>
2021-03-09 12:29:57 +0100

    prueba 2

--- models/hd_inherit.py
+++ models/hd_inherit.py
@@ -6,8 +6,8 @@ class helpdeskticket(models.Model):
     _inherit = 'helpdesk.ticket'
 
     campo_orden_sat = fields.Many2one('mrp.repair', ondelete='set null', string="Orden SAT")
-    product_id = fields.Many2one('product.product', ondelete='set null', string="Producto a reparar")
-    lot_id = fields.Many2one('stock.production.lot', ondelete='set null', string="Lote/Nº de serie")
+    product_id = fields.Many2one('stock.production.lot'', ondelete='set null', string="Producto a reparar")
+    lot_id = fields.Many2one('product.product', ondelete='set null', string="Lote/Nº de serie")
     
     
 
