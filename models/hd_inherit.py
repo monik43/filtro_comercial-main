edb2f43026df9869547b06206c090ee70edfc1c0
psat6 <psat6@penguin>
2021-03-09 11:23:52 +0100

    empiezo desarrollo get prod, lot, serial

--- models/hd_inherit.py
+++ models/hd_inherit.py
@@ -8,8 +8,6 @@ class helpdeskticket(models.Model):
     campo_orden_sat = fields.Many2one('mrp.repair', ondelete='set null', string="Orden SAT")
     product_id = fields.Many2one('product.product', ondelete='set null', string="Producto a reparar")
     lot_id = fields.Many2one('stock.production.lot', ondelete='set null', string="Lote/Nº de serie")
-    print(x_sn)
+    serial_n = fields.Char()
 
-class mrprepair(models.Model):
-    _inherit = 'mrp.repair'
-
+    
