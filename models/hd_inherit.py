1d3ba7d964a667986abd30abd0c83128325eeb77
psat6 <psat6@penguin>
2021-03-09 11:19:48 +0100

    ticked add context + model + fix

--- models/hd_inherit.py
+++ models/hd_inherit.py
@@ -8,6 +8,7 @@ class helpdeskticket(models.Model):
     campo_orden_sat = fields.Many2one('mrp.repair', ondelete='set null', string="Orden SAT")
     product_id = fields.Many2one('product.product', ondelete='set null', string="Producto a reparar")
     lot_id = fields.Many2one('stock.production.lot', ondelete='set null', string="Lote/Nº de serie")
+    print(x_sn)
 
 class mrprepair(models.Model):
     _inherit = 'mrp.repair'
