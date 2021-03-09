25bbedd00146e97228375d4619d2e265f1cc6ef5
psat6 <psat6@penguin>
2021-03-09 12:11:46 +0100

    prueba 2

--- models/hd_inherit.py
+++ models/hd_inherit.py
@@ -8,4 +8,8 @@ class helpdeskticket(models.Model):
     campo_orden_sat = fields.Many2one('mrp.repair', ondelete='set null', string="Orden SAT")
     product_id = fields.Many2one('product.product', ondelete='set null', string="Producto a reparar")
     lot_id = fields.Many2one('stock.production.lot', ondelete='set null', string="Lote/Nº de serie")
-    serial_n = fields.Char()
+    
+    for record in self:
+        record['x_sn'] = record.lot_id
+
+    print(lot_id)
