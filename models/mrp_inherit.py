631ffcbd55c3b232ffd63a7369ac2e4524f39f00
monikapsat6 <psat6@cloudeducation.net>
2021-03-18 09:38:08 +0000

    prueba 4 cambiando modelo en report

--- models/mrp_inherit.py
+++ models/mrp_inherit.py
@@ -6,4 +6,4 @@ class mrprepair(models.Model):
 
     @api.multi
     def action_etiqueta_impr(self):
-        return self.env.ref('helpdesk_ticket.report_etiqueta_sat').report_action(self)
+        return self.env.ref('mrp_repair.report_etiqueta_sat').report_action(self)
