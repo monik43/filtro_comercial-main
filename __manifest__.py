0821f35e6ab1a3014ef64c80ff733936fb8d05ac
monikapsat6 <psat6@cloudeducation.net>
2021-03-18 09:23:36 +0000

    actualizacion manifest

--- __manifest__.py
+++ __manifest__.py
@@ -17,6 +17,7 @@
     'data': [
         'views/res_partner_add_filter_view.xml',
         'views/mrp_repair_add_btn.xml',
+        'reports/sat_ticket_report.xml',
         #'views/hd_ticket_add_view.xml',
     ],
 }
