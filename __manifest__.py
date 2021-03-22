6cbc0f9cf6d81fe2445d6b869967dc96f556b10a
monikapsat6 <psat6@cloudeducation.net>
2021-03-22 08:29:52 +0000

    prueba action imprimir 'Etiqueta SAT'

--- __manifest__.py
+++ __manifest__.py
@@ -17,7 +17,6 @@
     'data': [
         'views/res_partner_add_filter_view.xml',
         'views/mrp_repair_add_btn.xml',
-        'report/mrp_repair_templates.xml',
         'report/mrp_repair_ticket_report.xml',
         #'views/hd_ticket_add_view.xml',
     ],
