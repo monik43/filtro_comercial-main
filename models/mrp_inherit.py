7d46b91cbf90e15bac48505d761e491cb45228d2
monikapsat6 <psat6@cloudeducation.net>
2021-03-24 10:28:25 +0000

    boton funcional en reparaciones

--- models/mrp_inherit.py
+++ models/mrp_inherit.py
@@ -7,10 +7,4 @@ class mrprepair(models.Model):
     @api.multi
     def report_etiqueta_sat_label(self):
         ticket = self.env['helpdesk.ticket'].browse(self.name[:4])
-
-
-        print(f'////////////////////////////////////////////////////////////////////////////// {ticket.id}')
-        print(self.name)
-
-        
         return self.env.ref('filtro_comercial.cd_report_etiqueta_sat').report_action(ticket)
