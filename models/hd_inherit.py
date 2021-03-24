4b7da3dde28589213272c79c2f6038e6b93a563b
monikapsat6 <psat6@cloudeducation.net>
2021-03-24 10:44:36 +0000

    pruebas formato template

--- models/hd_inherit.py
+++ models/hd_inherit.py
@@ -5,10 +5,6 @@ from odoo import models, fields, api
 class helpdeskticket(models.Model):
     _inherit = 'helpdesk.ticket'
 
-    for rec in self:
-        fecha_entrada = self.create_date[-8]
-    
-
     campo_orden_sat = fields.Many2one('mrp.repair', ondelete='set null', string="Orden SAT")
     product_id = fields.Many2one('stock.production.lot', ondelete='set null', string="Producto a reparar")
     lot_id = fields.Many2one('product.product', ondelete='set null', string="Lote/Nº de serie")
