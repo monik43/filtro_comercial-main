214a2afdfa69d06d9633f69f38f86039adce8017
monikapsat6 <psat6@cloudeducation.net>
2021-03-24 10:13:38 +0000

    prueba funcionamiento modelos 7

--- models/mrp_inherit.py
+++ models/mrp_inherit.py
@@ -6,12 +6,6 @@ class mrprepair(models.Model):
 
     @api.multi
     def report_etiqueta_sat_label(self):
-        for record in self:
-            tickets = self.env['helpdesk.ticket'].browse(self.name[:4])
-
-
-            print(f'////////////////////////////////////////////////////////////////////////////// {tickets.id}')
-            print(self.name)
-
         
-        #return self.env.ref('filtro_comercial.cd_report_etiqueta_sat').report_action(self)
+        ticket_id = self.env['helpdesk.ticket'].browse(self.name[:4])
+        return self.env.ref('filtro_comercial.cd_report_etiqueta_sat').report_action(self)
