924bd1d928386bad937751c7a2541b61ce149724
monikapsat6 <psat6@cloudeducation.net>
2021-03-26 10:34:53 +0000

    nuevo boton en stock, cambios en etiqueta sat

---
+++ models/stock_inherit.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+class stockpicking(models.Model):
+    _inherit = 'stock.picking'
+
+    @api.multi
+    def report_etiqueta_stock_label(self):
+        return self.env.ref('filtro_comercial-main.cd_report_etiqueta_stock').report_action(self)
