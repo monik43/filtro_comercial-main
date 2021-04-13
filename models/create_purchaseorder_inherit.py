990fe54593ee668553942c143301404dca0ea178
monikapsat6 <psat6@cloudeducation.net>
2021-04-13 12:07:38 +0200

    cambios necesarios en los metodos

--- models/create_purchaseorder_inherit.py
+++ models/create_purchaseorder_inherit.py
@@ -1,7 +1,59 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api
+import time
+from odoo import api, fields, models, _
+from datetime import datetime
+import odoo.addons.decimal_precision as dp
+from odoo.exceptions import UserError
+
 
 class createpurchaseordermrp(models.TransientModel):
     _inherit = 'create.purchaseorder_mrp'
 
-    warehouse = fields.Many2one('stock.picking.type', string='Almacén', required = True)
+    warehouse = fields.Many2one(
+        'stock.picking.type', string='Almacén', required=True)
+
+    @api.multi
+    def action_create_purchase_order_mrp(self):
+        self.ensure_one()
+        res = self.env['purchase.order'].browse(
+            self._context.get('id', []))
+        value = []
+        pricelist = self.partner_id.property_product_pricelist
+        partner_pricelist = self.partner_id.property_product_pricelist
+        mrp_repair_name = ""
+        for data in self.new_order_line_ids:
+            final_price = 00.0
+            mrp_repair_name = data.order_id.name
+            if partner_pricelist:
+                product_context = dict(
+                    self.env.context, partner_id=self.partner_id.id, date=self.date_order, uom=data.product_uom.id)
+
+                final_price, rule_id = partner_pricelist.with_context(product_context).get_product_price_rule(
+                    data.product_id, data.product_qty or 1.0, self.partner_id)
+#				base_price, currency = self.with_context(product_context)._get_real_price_currency(data.product_id, rule_id, data.product_qty, data.product_uom, partner_pricelist)
+
+            else:
+                final_price = data.product_id.standard_price
+
+        for data in self.new_order_line_ids:
+            value.append([0, 0, {
+                'product_id': data.product_id.id,
+                'name': data.name,
+                'product_qty': data.product_qty,
+                'order_id': data.order_id.id,
+                'product_uom': data.product_uom.id,
+                'taxes_id': ([(6, 0, data.product_id.supplier_taxes_id.ids)]),
+                'date_planned': datetime.today(),
+                'price_unit': final_price,
+            }])
+
+        res.create({
+            'partner_id': self.partner_id.id,
+            'date_order': self.date_order,
+            'order_line': value,
+            'origin': mrp_repair_name,
+            'partner_ref': mrp_repair_name,
+            'picking_type_id': self.warehouse
+        })
+        
+        return res
