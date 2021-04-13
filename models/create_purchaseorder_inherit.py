9b160cf340d3922ff44cf1726ac606b225114401
monikapsat6 <psat6@cloudeducation.net>
2021-04-13 10:49:29 +0200

    warehouse field introducido

--- models/create_purchaseorder_inherit.py
+++ models/create_purchaseorder_inherit.py
@@ -4,4 +4,4 @@ from odoo import models, fields, api
 class createpurchaseordermrp(models.Model):
     _inherit = 'create.purchaseorder_mrp'
 
-    
+    warehouse = fields.Many2one('stock.warehouse', string='Almacén', required = True)
