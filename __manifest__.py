07f7f1d6c7a79ceea8ade0436f44376219fb8986
monikapsat6 <psat6@cloudeducation.net>
2021-04-15 11:15:52 +0200

    archivos indexados en manifest

--- __manifest__.py
+++ __manifest__.py
@@ -13,16 +13,17 @@
     'version': '11.0.0.1',
 
     'depends': ['base', 'helpdesk', 'mrp_repair', 'cloudalia_purchase_from_repair', 'stock'],
-    
+
     'data': [
         'views/res_partner_add_filter_view.xml',
         'views/mrp_repair_add_btn.xml',
+        'views/hd_ticket_add_btn.xml',
         'views/stock_picking_add_btn.xml',
         'views/stock_warehouse_add_type.xml',
         'views/purchaseorder_mrp_add_fields.xml',
         'views/stock_picking_type_add_warranty.xml',
         'report/cloudalia_report_etiqueta_sat.xml',
         'report/cloudalia_report_etiqueta_stock.xml'
-        #'views/hd_ticket_add_view.xml',
+
     ],
 }
