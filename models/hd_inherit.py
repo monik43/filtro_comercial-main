caedf0fb4f58d915d6cbf812669281bf47855c2d
monikapsat6 <psat6@cloudeducation.net>
2021-04-15 11:15:38 +0200

    añadido boton imprimir etiqueta

--- models/hd_inherit.py
+++ models/hd_inherit.py
@@ -8,3 +8,7 @@ class helpdeskticket(models.Model):
     campo_orden_sat = fields.Many2one('mrp.repair', ondelete='set null', string="Orden SAT")
     product_id = fields.Many2one('stock.production.lot', ondelete='set null', string="Producto a reparar")
     lot_id = fields.Many2one('product.product', ondelete='set null', string="Lote/Nº de serie")
+
+    @api.multi
+    def report_etiqueta_sat_label(self):
+        return self.env.ref('filtro_comercial-main.cd_report_etiqueta_sat').report_action(self)
