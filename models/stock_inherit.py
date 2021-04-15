c80981a2a8a427ae829ef36cd0291b9894aa7eef
monikapsat6 <psat6@cloudeducation.net>
2021-04-15 10:07:57 +0200

    cambio en la forma en la que se llama a la orden de reparacion

--- models/stock_inherit.py
+++ models/stock_inherit.py
@@ -4,13 +4,10 @@ from odoo import models, fields, api
 class stockpicking(models.Model):
     _inherit = 'stock.picking'
 
-    orden = ""
+    
     @api.multi
     def report_etiqueta_stock_label(self):
-        orden = self.env['purchase.order'].search([('name','=',self.origin)])
         
-        print("//"*15,"ID DE STOCKPICKING: " , self.id)
-        print("//"*15,"ID DE ORDEN: " , orden.id)
         return self.env.ref('filtro_comercial-main.cd_report_etiqueta_stock').report_action(self)
     
 class stockwarehouse(models.Model):
