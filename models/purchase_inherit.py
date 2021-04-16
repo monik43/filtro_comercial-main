4f9d23c5eb22d32331f1b6cd1d6ddc540c46308a
monikapsat6 <psat6@cloudeducation.net>
2021-04-16 16:00:42 +0200

    pruebas desde modelo purchase order para modificar partner ref

---
+++ models/purchase_inherit.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+
+class purchaseorder(models.Model):
+    _inherit = 'purchase.order'
+
+    @api.multi
+    def report_etiqueta_purchase_order(self):
+
+        if 'partner_ref' in self.env['purchase.order']._fields & 'partner_ref_eti' not in self.env['purchase.order']._fields:
+            
+            self.partner_ref_eti = self.partner_ref
+
+            if self.partner_ref_eti.startswith('#'):
+
+                self.partner_ref_eti = self.partner_ref_eti[1:]
+
+            if len(self.partner_ref_eti) > 17:
+
+                self.partner_ref_eti = self.partner_ref_eti[:17]
+
+        return self.env.ref('cd_report_etiqueta_purchase_order').report_action(self)
