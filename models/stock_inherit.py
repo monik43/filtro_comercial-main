f7c35f145a5c1505553e2ff960d7e9b3fc3a080d
monikapsat6 <psat6@cloudeducation.net>
2021-04-20 12:46:25 +0200

    errores produccion corregidos

--- models/stock_inherit.py
+++ models/stock_inherit.py
@@ -10,11 +10,6 @@ class stockpicking(models.Model):
         purchase_order = self.env['purchase.order'].search([('picking_ids', '=', self.id)])
         return self.env.ref('filtro_comercial-main.cd_report_etiqueta_purchase_order').report_action(purchase_order)
     
-class stockwarehouse(models.Model):
-    _inherit = 'stock.warehouse'
-
-    warehouse_type = fields.Char()
-
 class stockpickingtype(models.Model):
     _inherit = 'stock.picking.type'
 
