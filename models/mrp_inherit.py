1c1e5ad45a813211a5b12f05cf3547c35de704a5
monikapsat6 <psat6@cloudeducation.net>
2021-04-28 09:21:51 +0200

    cambios no guardados

--- models/mrp_inherit.py
+++ models/mrp_inherit.py
@@ -4,7 +4,13 @@ from odoo import models, fields, api
 class mrprepair(models.Model):
     _inherit = 'mrp.repair'
 
+    #ticker_rel = fields.Many2one
+
     @api.multi
     def report_etiqueta_sat_label(self):
         ticket = self.env['helpdesk.ticket'].browse(self.name[:4])
+        # error en produccion - mirar otra manera de acceder al ID
+
+
+
         return self.env.ref('filtro_comercial-main.cd_report_etiqueta_sat').report_action(ticket)
