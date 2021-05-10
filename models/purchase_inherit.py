4a3d7591ccfa96052a0ee18ecb3e8ddb8ad328b6
monikapsat6 <psat6@cloudeducation.net>
2021-05-10 11:38:05 +0200

    correccion error del modelo

--- models/purchase_inherit.py
+++ models/purchase_inherit.py
@@ -6,11 +6,6 @@ class purchaseorder(models.Model):
     _inherit = 'purchase.order'
 
     partner_ref_eti = fields.Char()
-    move_state = fields.Char(compute='_assign_movement_state')
-
-    #@api.depends('move_ids')
-    def _assign_movement_state(self):
-        move_state = self.move_ids.state
 
     @api.multi
     def report_etiqueta_purchase_order(self):
@@ -27,3 +22,12 @@ class purchaseorder(models.Model):
                 self.partner_ref_eti = self.partner_ref_eti[:17]
 
         return self.env.ref('filtro_comercial-main.cd_report_etiqueta_purchase_order').report_action(self)
+
+class purchaseorderline(models.Model):
+    _inherit = 'purchase.order.line' 
+
+    move_state = fields.Char(compute='_assign_movement_state')
+
+    @api.depends('move_ids')
+    def _assign_movement_state(self):
+        move_state = self.move_ids.state
