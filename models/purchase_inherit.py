7f9955e048105d57e4033e0165caa616147eaa5a
monikapsat6 <psat6@cloudeducation.net>
2021-05-18 09:42:11 +0200

    prueba etiqueta partner ref

--- models/purchase_inherit.py
+++ models/purchase_inherit.py
@@ -5,22 +5,29 @@ from odoo import models, fields, api
 class purchaseorder(models.Model):
     _inherit = 'purchase.order'
 
-    partner_ref_eti = fields.Char()
+    partner_ref_eti = fields.Char(compute='_assign_printable_ref')
+
+    @api.depends('partner_ref')
+    def _assign_printable_ref(self):
+        if 'partner_ref' in self.env['purchase.order']._fields:
+            
+            for record in self:
+
+                self.partner_ref_eti = self.partner_ref
+
+                if self.partner_ref_eti.startswith('#'):
+
+                    self.partner_ref_eti = self.partner_ref_eti[1:]
+
+                if len(self.partner_ref_eti) > 17:
+
+                    self.partner_ref_eti = self.partner_ref_eti[:17]
+
+            print(self.partner_ref_eti)
 
     @api.multi
     def report_etiqueta_purchase_order(self):
-        if 'partner_ref' in self.env['purchase.order']._fields:
-            
-            self.partner_ref_eti = self.partner_ref
-
-            if self.partner_ref_eti.startswith('#'):
-
-                self.partner_ref_eti = self.partner_ref_eti[1:]
-
-            if len(self.partner_ref_eti) > 17:
-
-                self.partner_ref_eti = self.partner_ref_eti[:17]
-
+        
         return self.env.ref('filtro_comercial-main.cd_report_etiqueta_purchase_order').report_action(self)
 
 class purchaseorderline(models.Model):
